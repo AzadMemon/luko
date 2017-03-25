9a90ae1e253aa0de9132c4eed3fd7d63fff0e19a
Azad Memon <azad.memon@gmail.com>
2017-03-25 19:13:52 -0400

    Finished user flow. User can now confirm product nad have a relation that can be tracked

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,16 @@
 import {Router} from 'express'
 import config from './../config'
 import _ from 'lodash'
+import async from 'async'
 
+const User = require('./schema/user');
+const Product = require('./schema/product');
+const ProductUser = require('./schema/productUser');
 const request = require('request');
 const {OperationHelper} = require('apac');
 const getUrls = require('get-urls');
-const url = require('url');
-
+const nodeUrl = require('url');
+const bot = require('./../services/facebook/messengerbot');
 const router = new Router();
 const client_ca = new OperationHelper({
   awsId: config.awsAccessKeyId,
@@ -23,142 +27,358 @@ const client_us = new OperationHelper({
   locale: 'US'
 });
 
-router.get('/webhook', function (req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-    req.query['hub.verify_token'] === config.fbVerifyToken) {
-    console.log("Validating webhook")
-    res.status(200).send(req.query['hub.challenge'])
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.")
-    res.sendStatus(403)
-  }
+router.get('/*', (req, res) => {
+  return bot._verify(req, res)
 })
 
-router.post('/webhook', function (req, res) {
-  var data = req.body;
-
-  if (data.object === 'page') {
-    data.entry.forEach(function (entry) {
-      var pageID = entry.id;
-      var timeOfEvent = entry.time;
-
-      entry.messaging.forEach(function (event) {
-        if (event.message) {
-          receivedMessage(event);
-        } else if (event.postback) {
-          receivedPostback(event);
-        } else {
-          console.log("Webhook received unknown event: ", event);
-        }
-
-        // TODO: Track when a user has read the message and when the message has been delivered
-      });
-    });
-
-    res.sendStatus(200);
-  }
+router.post('/*', (req, res) => {
+  bot._handleMessage(req.body)
+  res.end(JSON.stringify({status: 'ok'}))
 })
 
+bot.on('error', (err) => {
+  console.log(err.message)
+})
 
-function receivedMessage(event) {
-  var senderID = event.sender.id;
-  var message = event.message;
-  var messageText = message.text;
+bot.on('message', function (payload, reply, actions) {
+  var senderId = payload.sender.id;
+  var messageText = payload.message.text;
 
-  var links = getUrls(messageText);
-    if (links.size) {
-    if (links.size > 1) {
+  var urls = getUrls(messageText);
+  if (urls.size) {
+    if (urls.size > 1) {
       // TODO: Maybe iterate over links to service all links?
-      return sendErrorTextMessage(senderID);
+      return sendErrorTextMessage(senderId);
     }
 
-    var amazonLink = Array.from(links)[0];
-    var amazonASIN = extractASIN(amazonLink);
+    var amazonUrl = Array.from(urls)[0];
+    var amazonASIN = extractASIN(amazonUrl);
 
     if (!amazonASIN) {
-      return sendErrorTextMessage(senderID);
+      return sendErrorTextMessage(senderId);
     }
 
-    var client;
-    if (amazonLink.includes('amazon.com')) {
-      client = client_us;
-    } else if (amazonLink.includes('amazon.ca')) {
-      client = client_ca;
-    } else {
-      return sendWrongCountryTextMessage(senderID);
+    var client = getClientByUrl(amazonUrl)
+
+    if (!client) {
+      return sendWrongCountryTextMessage(senderId);
     }
 
-    return client.execute('ItemLookup', {
-      IdType: 'ASIN',
-      ItemId: amazonASIN,
-      ResponseGroup: 'Images, ItemAttributes, OfferFull'
-    }).then(function(results) {
-      if (_.get(results, 'result.ItemLookupResponse.Items.Request.Errors.Error')) {
-        return sendIllFormedTextMessage(senderID);
-      }
+    return getAmazonProduct(amazonASIN, client)
+      .then(function (results) {
+        if (_.get(results, 'result.ItemLookupResponse.Items.Request.Errors.Error')) {
+          return sendIllFormedTextMessage(senderId);
+        }
 
-      sendConfirmTrackMessage(senderID, results);
-    }).catch(function(error) {
-      sendErrorTextMessage(senderID);
-    });
+        sendConfirmTrackMessage(senderId, results);
+      }).catch(function (error) {
+        sendErrorTextMessage(senderId);
+      });
   }
 
   if (messageText) {
     switch (messageText) {
       case 'Manage My Products':
-        // TODO: send something
+      // TODO: send something
       case 'Help':
-        // TODO: send something
+      // TODO: send something
       case 'Add a Product':
       // TODO: send something
       default:
-        return sendErrorTextMessage(senderID);
+        return sendErrorTextMessage(senderId);
     }
   }
 
-  sendErrorTextMessage(senderID);
-}
+  sendErrorTextMessage(senderId);
+});
 
-function sendIntroMessage(senderID) {
+bot.on('postback', (payload, reply, actions) => {
+  var senderId = payload.sender.id;
+  var payload = payload.postback.payload;
+
+  if (payload.includes('Track')) {
+    var info = payload.split(":::");
+    var asin = info[1];
+    var url = info[2];
+
+    trackProduct(senderId, asin, url);
+  } else if (payload == 'Get Started') {
+    createUser(senderId);
+    sendIntroMessage(senderId);
+  } else if (payload == 'Manage My Products') {
+    // TODO: Return something
+  } else if (payload == 'Add a Product') {
+    // TODO: Return something
+  } else if (payload == 'Help') {
+    // TODO: Return something
+  }
+})
+
+function sendIntroMessage(recipientId) {
   sendTextMessage(
-    senderID,
+    recipientId,
     "Hey! I’m Luko, a price tracking bot for Amazon products. " +
     "Have a product you’re interested in but you aren’t willing to pay? " +
     "No PROBLEM! Simply paste the product link here and I’ll message you if the price drops."
   );
 }
 
-function sendWrongCountryTextMessage(recipientID) {
+function sendWrongCountryTextMessage(recipientId) {
   sendTextMessage(
-    recipientID,
+    recipientId,
     "I’m sorry, but I currently only work with Amazon's Canada and U.S. stores. I’ve noted that you’re interested and " +
     "I’ll be sure to notify you when I've learnt how to work with stores in that area!"
   )
 }
 
-function sendIllFormedTextMessage(recipientID) {
+function sendIllFormedTextMessage(reply) {
   sendTextMessage(
-    recipientID,
+    reply,
     "I’m sorry, I couldn’t find that product, are you sure your link is correct?"
   );
 }
-function sendErrorTextMessage(recipientID) {
+
+function sendErrorTextMessage(recipientId) {
   sendTextMessage(
-    recipientID,
+    recipientId,
     "Sorry I didn't quite understand that! I only speak in Amazon Links. " +
     "Can you try pasting that Amazon link again?"
   );
 }
 
-function kindleEbookNotSupportedMessage(recipientID) {
+function kindleEbookNotSupportedMessage(recipientId) {
   sendTextMessage(
-    recipientID,
+    recipientId,
     "Sorry, but at this time, Amazon doesn't let me track Kindle books. Try pasting a different product link!"
   );
 }
 
-function extractASIN(link) {
-  var parsedUrl = url.parse(decodeURIComponent(link));
+function trackProduct(userId, asin, url) {
+  async.waterfall([
+    retrieveProduct,
+    getProduct,
+    getUser,
+    addTrackingRelationship
+  ], finalCallback);
+
+  function retrieveProduct(waterfallNext) {
+    var client = getClientByUrl(url);
+
+    getAmazonProduct(asin, client)
+      .then(function (amazonResult) {
+        if (_.get(amazonResult, 'result.ItemLookupResponse.Items.Request.Errors.Error')) {
+          return waterfallNext(new Error('Amazon returned an error'));
+        }
+
+        waterfallNext(null, amazonResult);
+      })
+      .catch(function (error) {
+        waterfallNext(error);
+      });
+  }
+
+  function getProduct(amazonResult, waterfallNext) {
+    var store = getStoreByUrl(url);
+    var detailPageUrl = _.get(amazonResult, 'result.ItemLookupResponse.Items.Item.DetailPageURL');
+    var currencyCode = _.get(amazonResult, 'result.ItemLookupResponse.Items.Item.OfferSummary.LowestNewPrice.CurrencyCode');
+    var amount = _.get(amazonResult, 'result.ItemLookupResponse.Items.Item.OfferSummary.LowestNewPrice.Amount');
+    var formattedAmount = _.get(amazonResult, 'result.ItemLookupResponse.Items.Item.OfferSummary.LowestNewPrice.FormattedPrice');
+    var largeImageUrl = _.get(amazonResult, 'result.ItemLookupResponse.Items.Item.LargeImage.URL');
+    var mediumImageUrl = _.get(amazonResult, 'result.ItemLookupResponse.Items.Item.MediumImage.URL');
+    var smallImageUrl = _.get(amazonResult, 'result.ItemLookupResponse.Items.Item.SmallImage.URL');
+    var publisher = _.get(amazonResult, 'result.ItemLookupResponse.Items.Item.ItemAttributes.Publisher');
+    var title = _.get(amazonResult, 'result.ItemLookupResponse.Items.Item.ItemAttributes.Title');
+
+    Product.findOneAndUpdate(
+      {
+        store: getStoreByUrl(url),
+        asin: asin
+      },
+      {
+        link: detailPageUrl,
+        asin: asin,
+        currentPrice: {
+          amount: amount,
+          formattedAmount: formattedAmount,
+          currencyCode: currencyCode
+        },
+        $push: {
+          priceHistory: {
+            date: Date.now(),
+            amount: amount,
+            formattedAmount: formattedAmount,
+            currencyCode: currencyCode
+          }
+        },
+        store: store,
+        imageUrl: largeImageUrl || mediumImageUrl || smallImageUrl,
+        title: title,
+        seller: publisher
+      },
+      {upsert: true, new: true},
+      waterfallNext
+    );
+  }
+
+  function getUser(product, waterfallNext) {
+    User.findOne(
+      {fbUserId: userId},
+      function (error, user) {
+        if (error) {
+          return waterfallNext(error);
+        } else if (!user) {
+          return waterfallNext(new Error("Couldn't find user in database"));
+        }
+
+        waterfallNext(null, user, product);
+      });
+  }
+
+  function addTrackingRelationship(user, product, waterfallNext) {
+    ProductUser.findOneAndUpdate(
+      {
+        productId: product._id,
+        userId: user._id
+      },
+      {
+        productId: product._id,
+        userId: user._id,
+        thresholdPrice: {
+          amount: product.currentPrice.amount,
+          formattedAmount: product.currentPrice.formattedAmount,
+          currencyCode: product.currentPrice.currencyCode
+        },
+        isTracking: true,
+        lastNotified: Date.now()
+      },
+      {upsert: true, new: true},
+      waterfallNext
+    )
+  }
+
+  function finalCallback(error, result) {
+    if (error) {
+      console.log(error);
+      return sendTextMessage(userId, "Ooops, something went wrong with my magical amazon commmunication skills. Try again in a bit!");
+    }
+
+    return sendTextMessage(userId, "Great, I’ll let you know when the price drops. Remember you can always just paste a link for the next product you want to track!");
+  }
+}
+
+function createUser(userId) {
+  async.waterfall([
+    getProfile,
+    findOrCreateUser
+  ], finalCallback);
+
+  function getProfile(waterfallNext) {
+    bot.getProfile(userId, function (error, profile) {
+      if (error) {
+        return waterfallNext(error);
+      }
+
+      waterfallNext(null, profile);
+    })
+  }
+
+  function findOrCreateUser(profile, waterfallNext) {
+    User.findOne(
+      {fbUserId: userId},
+      {
+        fbUserId: userId,
+        firstName: profile.first_name,
+        lastName: profile.last_name,
+        timezone: profile.timezone,
+        gender: profile.gender,
+        locale: profile.locale
+      },
+      {upsert: true, new: true},
+      function (error, result) {
+        if (error) {
+          return waterfallNext(error);
+        }
+
+        waterfallNext(null, result);
+      });
+  }
+
+  function finalCallback(error) {
+    if (error) {
+      console.log(error);
+    }
+  }
+}
+
+function sendTextMessage(recipientId, messageText) {
+  bot.sendMessage(recipientId, {text: messageText});
+}
+
+function sendConfirmTrackMessage(recipientId, results) {
+  var detailPageUrl = _.get(results, 'result.ItemLookupResponse.Items.Item.DetailPageURL');
+  var lowestNewPrice = _.get(results, 'result.ItemLookupResponse.Items.Item.OfferSummary.LowestNewPrice.FormattedPrice');
+  var largeImageUrl = _.get(results, 'result.ItemLookupResponse.Items.Item.LargeImage.URL');
+  var mediumImageUrl = _.get(results, 'result.ItemLookupResponse.Items.Item.MediumImage.URL');
+  var smallImageUrl = _.get(results, 'result.ItemLookupResponse.Items.Item.SmallImage.URL');
+  var publisher = _.get(results, 'result.ItemLookupResponse.Items.Item.ItemAttributes.Publisher');
+  var title = _.get(results, 'result.ItemLookupResponse.Items.Item.ItemAttributes.Title');
+  var format = _.get(results, 'result.ItemLookupResponse.Items.Item.ItemAttributes.Format');
+
+  if (format && format.toLowerCase() == 'kindle ebook') {
+    return kindleEbookNotSupportedMessage(recipientId);
+  }
+
+  bot.sendMessage(recipientId, {
+    attachment: {
+      type: "template",
+      payload: {
+        template_type: "generic",
+        elements: [{
+          title: title,
+          subtitle: publisher + ' - ' + lowestNewPrice,
+          item_url: detailPageUrl,
+          image_url: largeImageUrl || mediumImageUrl || smallImageUrl,
+          buttons: [{
+            type: "web_url",
+            url: detailPageUrl,
+            title: "View Product"
+          }, {
+            type: "postback",
+            title: "Track",
+            payload: "Track:::" + _.get(results, 'result.ItemLookupResponse.Items.Item.ASIN') + ":::" + detailPageUrl
+          }]
+        }]
+      }
+    }
+  });
+}
+
+function getClientByUrl(url) {
+  if (url.includes('amazon.com')) {
+    return client_us;
+  } else if (url.includes('amazon.ca')) {
+    return client_ca;
+  }
+}
+
+function getStoreByUrl(url) {
+  if (url.includes('amazon.com')) {
+    return 'amazon.com'
+  } else if (url.includes('amazon.ca')) {
+    return 'amazon.ca'
+  }
+}
+
+function getAmazonProduct(asin, client) {
+  return client.execute('ItemLookup', {
+    IdType: 'ASIN',
+    ItemId: asin,
+    ResponseGroup: 'Images, ItemAttributes, OfferFull'
+  })
+}
+
+function extractASIN(url) {
+  var parsedUrl = nodeUrl.parse(decodeURIComponent(url));
 
   if (parsedUrl != null && parsedUrl.hostname != null && parsedUrl.hostname.includes("amazon")) {
     var paths = parsedUrl.pathname.split('/');
@@ -168,119 +388,6 @@ function extractASIN(link) {
   }
 
   return '';
-}
-
-
-function receivedPostback(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfPostback = event.timestamp;
-
-  var payload = event.postback.payload;
-
-
-  if (payload == 'Get Started') {
-    return sendIntroMessage(senderID);
-  } else if (payload == 'Manage My Products') {
-    // TODO: Return something
-  } else if (payload == 'Add a Product') {
-    // TODO: Return something
-  } else if (payload == 'Help') {
-    // TODO: Return something
-  }
-}
-
-
-function sendConfirmTrackMessage(recipientID, results) {
-
-  var detailPageUrl = _.get(results, 'result.ItemLookupResponse.Items.Item.DetailPageURL');
-  var lowestNewPrice = _.get(results, 'result.ItemLookupResponse.Items.Item.OfferSummary.LowestNewPrice');
-  var largeImage = _.get(results, 'result.ItemLookupResponse.Items.Item.LargeImage');
-  var itemAttributes = _.get(results, 'result.ItemLookupResponse.Items.Item.ItemAttributes');
-
-
-  if (_.get(itemAttributes, 'Format').toLowerCase() == 'kindle ebook') {
-    return kindleEbookNotSupportedMessage(recipientID);
-  }
-
-  var messageData = {
-    recipient: {
-      id: recipientID
-    },
-    message: {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: [{
-            title: itemAttributes.Title,
-            subtitle: itemAttributes.Publisher + ' - ' + lowestNewPrice.FormattedPrice,
-            item_url: detailPageUrl,
-            image_url: largeImage.URL,
-            buttons: [{
-              type: "web_url",
-              url: detailPageUrl,
-              title: "View Product"
-            }, {
-              type: "postback",
-              title: "Track",
-              payload: JSON.stringify({
-                cta: 'track',
-                asin: _.get(results, 'result.ItemLookupResponse.Items.Item.ASIN')
-              })
-            }]
-          }]
-        }
-      }
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-function trackPackage(userID, asin) {
-  // var detailPageUrl = results.result.ItemLookupResponse.Items.Item.DetailPageURL;
-  // var lowestNewPrice = results.result.ItemLookupResponse.Items.Item.OfferSummary.LowestNewPrice;
-  // var lowestUsedPrice = results.result.ItemLookupResponse.Items.Item.OfferSummary.LowestUsedPrice;
-  // var largeImage = results.result.ItemLookupResponse.Items.Item.LargeImage;
-  // var mediumImage = results.result.ItemLookupResponse.Items.Item.MediumImage;
-  // var smallImage = results.result.ItemLookupResponse.Items.Item.SmallImage;
-  // var itemAttributes = results.result.ItemLookupResponse.Items.Item.ItemAttributes;
-}
-
-function sendTextMessage(recipientID, messageText) {
-  var messageData = {
-    recipient: {
-      id: recipientID
-    },
-    message: {
-      text: messageText
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-function callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: config.pageAccessToken },
-    method: 'POST',
-    json: messageData
-
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-
-      console.log("Successfully sent generic message with id %s to recipient %s",
-        messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      console.error(response);
-      console.error(error);
-    }
-  });
 }
 
 export default router
